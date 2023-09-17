557f085ecc14f136b17e08c21e0d16bcfae8c8db
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-09-17 23:11:45 +0100

    update

---
+++ src/script.js
@@ -0,0 +1,22 @@
+/* Referces */
+
+let optbutton = document.querySelectorAll(".opt-btn");
+let advoptbutton = document.querySelectorAll(".adv-opt-btn");
+let fontname = document.getElementById("FontName");
+let fontsizeref = document.getElementById("FontSize");
+let textarea = document.getElementById("text-input");
+let linkbtn =  document.getElementById("linktext");
+let alignbtn = document.querySelectorAll(".align");
+let spacingbtn = document.querySelectorAll(".spacing");
+let formatbtn = document.querySelectorAll(".format");
+let scriptbtn = document.querySelectorAll(".script");
+////font style options
+let FontList = [
+    "Arial",
+    "Verdana",
+    "Times New Roman",
+    "Garamond",
+    "Georgia",
+    "Courier New",
+    "cursive",
+  ];
